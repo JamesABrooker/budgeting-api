3b9769ffaa84235f039b115863a82be948d084e0
brookejame <brookejame@myvuw.ac.nz>
2026-02-05 16:51:58 +1300

    Initial Commit

---
+++ src/test/java/com/jamesbrooker/budgeting/BudgetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesbrooker.budgeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
