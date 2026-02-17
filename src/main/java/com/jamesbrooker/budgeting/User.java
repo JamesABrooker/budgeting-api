87bc077de801723cf6ce060c33cd8f644bd58036
brookejame <brookejame@myvuw.ac.nz>
2026-02-17 11:03:05 +1100

    Added User class, connects fields to SQL table, handles unique emails, auto-generated IDs, and Timestamps

---
+++ src/main/java/com/jamesbrooker/budgeting/User.java
@@ -0,0 +1,53 @@
+package com.jamesbrooker.budgeting;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(
+        name = "users",
+        uniqueConstraints = { @UniqueConstraint(columnNames = "email") }
+)
+
+//User class holds all necessary information about the user
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    //Constructor
+    protected User(String email, String password) {
+        this.email = email;
+        this.password = hashPassword(password);
+        this.id = UUID.randomUUID();
+        this.createdAt = LocalDateTime.now();
+    }
+
+    //Default Constructor required by JPA
+    protected User() {}
+
+    //Password hashing placeholder
+    private String hashPassword(String password) {
+        return "";
+    }
+
+    // Getters & setters (needed for JPA to access fields)
+    public UUID getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+}
