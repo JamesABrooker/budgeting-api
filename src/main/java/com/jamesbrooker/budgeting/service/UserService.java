32f25b738e81649abef7adc2688ca20707f76f80
brookejame <brookejame@myvuw.ac.nz>
2026-02-17 11:42:36 +1100

    Added an update password function, and a validPassword function, to check if a given password is valid

--- src/main/java/com/jamesbrooker/budgeting/service/UserService.java
+++ src/main/java/com/jamesbrooker/budgeting/service/UserService.java
@@ -18,10 +18,15 @@ public class UserService {
 
     //Registers a new user
     public User register(String email, String rawPassword) {
+        //Checks that the email doesn't already exist
         if (userRepository.findByEmail(email).isPresent()) {
             throw new RuntimeException("Email already exists");
         }
 
+        //Checks that the password is valid
+        if(!validPassword(rawPassword)) {throw new RuntimeException("Invalid password");}
+
+        //Hashes password, creates a new user with the email and hashed password, and adds it to the repository
         String hashedPassword = passwordEncoder.encode(rawPassword);
         User user = new User(email, hashedPassword);
         return userRepository.save(user);
@@ -35,5 +40,19 @@ public class UserService {
         if (!passwordEncoder.matches(rawPassword, user.getPassword())) throw new RuntimeException("Invalid email or password");
         //Successfully logged in
         return user;
+    }
+
+    //Updates password for user
+    public User updatePassword(User user, String newRawPassword){
+        if (validPassword(newRawPassword)) {
+            user.setPassword(passwordEncoder.encode(newRawPassword));
+            return userRepository.save(user);
+        }
+        return user;
+    }
+
+    //Function to check if a given password is a valid password
+    private Boolean validPassword(String rawPassword) {
+        return rawPassword != null && !rawPassword.isBlank();
     }
 }
