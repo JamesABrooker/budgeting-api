5bccc89d0f23857c0cb4c0c56aee28cb81a69388
brookejame <brookejame@myvuw.ac.nz>
2026-02-17 11:36:17 +1100

    Added login function, correctly checks that the users email exists and that the passwords match

--- src/main/java/com/jamesbrooker/budgeting/service/UserService.java
+++ src/main/java/com/jamesbrooker/budgeting/service/UserService.java
@@ -10,11 +10,13 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    //Constructor
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
+    //Registers a new user
     public User register(String email, String rawPassword) {
         if (userRepository.findByEmail(email).isPresent()) {
             throw new RuntimeException("Email already exists");
@@ -23,5 +25,15 @@ public class UserService {
         String hashedPassword = passwordEncoder.encode(rawPassword);
         User user = new User(email, hashedPassword);
         return userRepository.save(user);
+    }
+
+    //Attempts to login a user, checks that the email exists and that the password is correct
+    public User login(String email, String rawPassword) {
+        //Checks that the email exists in the repository
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("Invalid email or password"));
+        //Checks that the passwords match
+        if (!passwordEncoder.matches(rawPassword, user.getPassword())) throw new RuntimeException("Invalid email or password");
+        //Successfully logged in
+        return user;
     }
 }
